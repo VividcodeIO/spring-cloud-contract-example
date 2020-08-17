8086ee8793ee8d742b738f73c093ab9a573c336d
Fu Cheng <alexcheng1982@gmail.com>
2020-08-17 20:50:23 +1200

    code format

--- producer/src/test/resources/contracts/trip/tripValidationFailed.groovy
+++ producer/src/test/resources/contracts/trip/tripValidationFailed.groovy
@@ -3,23 +3,23 @@ package contracts.trip
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-    request {
-        method 'POST'
-        url '/trip_validation'
-        body([
-                amount: 1100.00
-        ])
-        headers {
-            contentType('application/json')
-        }
+  request {
+    method 'POST'
+    url '/trip_validation'
+    body([
+        amount: 1100.00
+    ])
+    headers {
+      contentType('application/json')
     }
-    response {
-        status OK()
-        body([
-                valid: false
-        ])
-        headers {
-            contentType('application/json')
-        }
+  }
+  response {
+    status OK()
+    body([
+        valid: false
+    ])
+    headers {
+      contentType('application/json')
     }
+  }
 }
--- producer/src/test/resources/contracts/trip/tripValidationPassed.groovy
+++ producer/src/test/resources/contracts/trip/tripValidationPassed.groovy
@@ -3,23 +3,23 @@ package contracts.trip
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-    request {
-        method 'POST'
-        url '/trip_validation'
-        body([
-                amount: 100.00
-        ])
-        headers {
-            contentType('application/json')
-        }
+  request {
+    method 'POST'
+    url '/trip_validation'
+    body([
+        amount: 999.00
+    ])
+    headers {
+      contentType('application/json')
     }
-    response {
-        status OK()
-        body([
-                valid: true
-        ])
-        headers {
-            contentType('application/json')
-        }
+  }
+  response {
+    status OK()
+    body([
+        valid: true
+    ])
+    headers {
+      contentType('application/json')
     }
+  }
 }
